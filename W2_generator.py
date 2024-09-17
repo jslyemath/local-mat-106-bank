a446325072ac289cc70f9d0f940665c929430fb2
Jeff Slye <jslyemath@gmail.com>
2024-09-17 11:18:21 -0400

    Hotfix for W2 when before progress point 1. Desperately needs refactored at some point.

--- W2_generator.py
+++ W2_generator.py
@@ -3,6 +3,10 @@ import random
 
 
 def generate(**kwargs):
+    sub_allowed = False
+    if int(kwargs['course_progress']) > 0:
+        sub_allowed = True
+
     base_ten_num1 = ''
     base_ten_num2 = ''
     base_b_num1 = ''
@@ -14,8 +18,8 @@ def generate(**kwargs):
     base_b_base = random.choice((4, 5, 6, 7, 8, 9))
     base_names = ['null', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten']
     base_b_ss = base_names[base_b_base]
-    base_ten_op = random.choice(('+', '-'))
-    base_b_op = ''
+    base_ten_op = random.choice(('+', '-')) if sub_allowed else '+'
+    base_b_op = '+'
     xmore_likely = 4
     sub_match_counter = 0
     bt_n1_wts = {'wt_0': 0.0, 'wt_1': 0.0, 'wt_2': 0.0, 'wt_3': 0.0, 'wt_4': 0.0,
@@ -27,7 +31,45 @@ def generate(**kwargs):
     bb_n2_wts = {'wt_0': 0.0, 'wt_1': 0.0, 'wt_2': 0.0, 'wt_3': 0.0, 'wt_4': 0.0,
                  'wt_5': 0.0, 'wt_6': 0.0, 'wt_7': 0.0, 'wt_8': 0.0, 'wt_9': 0.0}
 
-    if base_ten_op == '+':
+    if not sub_allowed:
+        base_ten_op = '+'
+        base_b_op = '+'
+
+        for i, wt in enumerate(bt_n1_wts):
+            if i < 5:
+                bt_n1_wts[wt] = 1 / (10 * (xmore_likely + 1))
+            if 5 <= i < 10:
+                bt_n1_wts[wt] = xmore_likely / (10 * (xmore_likely + 1))
+
+        for i, wt in enumerate(bt_n2_wts):
+            if i < 5:
+                bt_n2_wts[wt] = 1 / (10 * (xmore_likely + 1))
+            if 5 <= i < 10:
+                bt_n2_wts[wt] = xmore_likely / (10 * (xmore_likely + 1))
+
+        base_ten_num1 = sm.int_string(place_values=4, **bt_n1_wts)
+        base_ten_num2 = sm.int_string(place_values=4, **bt_n2_wts)
+        base_ten_alg = random.choice(add_algs)
+        remaining_add_algs = list(add_algs)
+        remaining_add_algs.remove(base_ten_alg)
+
+        for i, wt in enumerate(bb_n1_wts):
+            if i < base_b_base / 2:
+                bb_n1_wts[wt] = xmore_likely / (base_b_base * (xmore_likely + 1))
+            if base_b_base / 2 <= i < base_b_base:
+                bb_n1_wts[wt] = 1 / (base_b_base * (xmore_likely + 1))
+
+        for i, wt in enumerate(bb_n2_wts):
+            if i < base_b_base / 2:
+                bb_n2_wts[wt] = 1 / (base_b_base * (xmore_likely + 1))
+            if base_b_base / 2 <= i < base_b_base:
+                bb_n2_wts[wt] = xmore_likely / (base_b_base * (xmore_likely + 1))
+
+        base_b_num1 = sm.int_string(place_values=4, **bb_n1_wts)
+        base_b_num2 = sm.int_string(place_values=4, **bb_n2_wts)
+        base_b_alg = random.choice(remaining_add_algs)
+
+    elif base_ten_op == '+':
         base_b_op = '-'
 
         for i, wt in enumerate(bt_n1_wts):
@@ -74,16 +116,6 @@ def generate(**kwargs):
 
         base_b_num2 = base_b_num2.lstrip('0')
 
-        # while True:
-        #     base_b_num2 = sm.int_string(place_values=4,
-        #                                 excl_first=excl_list,
-        #                                 **bb_n2_wts)
-
-        #     print(base_b_num2)
-
-        #     if int(base_b_num1) > int(base_b_num2):
-        #         break
-
         base_b_alg = random.choice(sub_algs)
 
     else:
@@ -102,19 +134,6 @@ def generate(**kwargs):
                 bt_n2_wts[wt] = xmore_likely / (10 * (xmore_likely + 1))
 
         base_ten_num1 = sm.int_string(place_values=4, **bt_n1_wts)
-
-        # excl_list = list(range(int(base_ten_num1[0]) + 1, 9))
-        # excl_list.append(0)
-
-        # while True:
-        #     base_ten_num2 = sm.int_string(place_values=4,
-        #                                 excl_first=excl_list,
-        #                                 **bt_n2_wts)
-
-        #     print(base_ten_num2)
-
-        #     if int(base_ten_num1) > int(base_ten_num2):
-        #         break
 
         for place, numeral in enumerate(base_ten_num1):
             if sub_match_counter == place:
